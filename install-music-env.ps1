662fd3f26c0e0f7af6665c2d1ae85ec79279909d
ibleedicare <ibleedicare@gmail.com>
2023-10-26 23:51:54 +0200

    Update install-music-env.ps1

---
+++ install-music-env.ps1
@@ -0,0 +1,6 @@
+choco install reaper --yes
+choco install GainReduction2 -s .\
+choco install Soothe2 -s .\
+choco install SerumXfer -s .\
+choco install SerumXfer -s .\
+choco install WaveCentral -s .\
